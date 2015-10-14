8d2601dc654636ca372720598fcf39a80381a47a
Patrick Schoeps <pschoeps@umich.edu>
2015-10-13 21:06:08 -0400

    ok

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,20 +1,20 @@
 class Api::V1::SessionsController < Devise::SessionsController
 
-  prepend_before_filter :require_no_authentication, :only => [:create ]
+  prepend_before_filter :require_no_authentication, :only => [:create]
   #include Devise::Controllers::InternalHelpers
   
-  before_filter :ensure_params_exist
+  #before_filter :ensure_params_exist
 
   respond_to :json
   
   def create
-    build_resource
-    resource = User.find_for_database_authentication(:login=>params[:user_login][:login])
-    return invalid_login_attempt unless resource
+    #build_resource
+    resource = User.find_for_database_authentication(email: user_params[:email])
+    return invalid_login_attempt_2 unless resource
 
-    if resource.valid_password?(params[:user_login][:password])
+    if resource.valid_password?(user_params[:password])
       sign_in("user", resource)
-      render :json=> {:success=>true, :auth_token=>resource.authentication_token, :login=>resource.login, :email=>resource.email}
+      render :json=> {:success=>true, :auth_token=>resource.auth_token, :email=>resource.email}
       return
     end
     invalid_login_attempt
@@ -25,13 +25,29 @@ class Api::V1::SessionsController < Devise::SessionsController
   end
 
   protected
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = SecureRandom.uuid
+    end while self.class.exists?(auth_token: auth_token)
+  end
+
   def ensure_params_exist
-    return unless params[:user_login].blank?
-    render :json=>{:success=>false, :message=>"missing user_login parameter"}, :status=>422
+    return unless params[:email].blank? 
+    render :json=>{:success=>false, :message=>"missing email or password"}, :status=>422
   end
 
   def invalid_login_attempt
     warden.custom_failure!
-    render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
+    render :json=> {:success=>false, :message=>"Invalid Password"}, :status=>401
   end
+
+   def invalid_login_attempt_2
+    warden.custom_failure!
+    render :json=> {:success=>false, :message=>"Invalid Email"}, :status=>401
+  end
+
+  def user_params
+      params.fetch(:user, {}).permit(:email, :password)
+    end
 end
