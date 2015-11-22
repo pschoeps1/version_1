2b8060e006dfb5b6f67b52d78b460d350ac77cc3
Patrick Schoeps <pschoeps@umich.edu>
2015-11-22 17:12:24 -0500

    ok

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -35,7 +35,7 @@ class Api::V1::UsersController < ApplicationController
     user = User.find_by_auth_token(user_params[:auth_token])
     groups = user.groups
     #followed_groups = user.following
-    render json: {groups: groups + followed_groups}
+    render json: { groups: groups }
     #respond_to do |format|
    # format.json  { render :json => {:groups=> groups, 
    #                               :followed_groups => followed_groups }}
