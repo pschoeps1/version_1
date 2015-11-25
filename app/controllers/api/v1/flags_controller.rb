0fafac77e0a93e6ddb9aafe5b85d207036114959
Patrick Schoeps <pschoeps@umich.edu>
2015-11-25 18:29:12 -0500

    ok

--- app/controllers/api/v1/flags_controller.rb
+++ app/controllers/api/v1/flags_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::FlagsController < ApplicationController
+
   def create
     flag = Flag.new
     resource = User.find_by_auth_token(auth_token: params[:auth_token])
     flag.user_id = resource.id
     flag.reporter_id = params[:reporter_id]
     flag.content = params[:content]
-    flag.user_name = params[:user_name]
 
     if flag.save
       render :json=> {:success=>true}
