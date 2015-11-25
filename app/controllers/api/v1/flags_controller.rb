a28966738a6f3ebb3e4c87adf40946c80c710f44
Patrick Schoeps <pschoeps@umich.edu>
2015-11-25 18:34:22 -0500

    oK

--- app/controllers/api/v1/flags_controller.rb
+++ app/controllers/api/v1/flags_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::FlagsController < ApplicationController
 	respond_to :json
 
   def create
-    flag = Flag.new
+    flag = Flag.new(flag_params)
     resource = User.find_by_auth_token(auth_token: params[:auth_token])
     flag.user_id = resource.id
     flag.reporter_id = params[:reporter_id]
@@ -17,5 +17,9 @@ class Api::V1::FlagsController < ApplicationController
   
 
   def show
+  end
+
+  def flag_params
+      params.require(:flag).permit(:user_id, :reporter_id, :auth_token, :content)
   end
 end
