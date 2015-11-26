01f76d8b570e39d43f9c87da7056951d3a278a26
Patrick Schoeps <pschoeps@umich.edu>
2015-11-25 19:01:52 -0500

    ok

--- app/controllers/api/v1/flags_controller.rb
+++ app/controllers/api/v1/flags_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::FlagsController < ApplicationController
 
   def create
     flag = Flag.new
-    resource = User.find_by_auth_token(auth_token: params[:auth_token])
+    resource = User.find_by_auth_token(params[:auth_token])
     flag.user_id = resource.id
     flag.reporter_id = params[:reporter_id]
     #flag.content = params[:content]
