61184b208600756805067e81ae21f8783a6e84bb
Patrick Schoeps <pschoeps@umich.edu>
2015-11-28 19:46:24 -0500

    ok

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GroupsController < ApplicationController
 	def show
-	  @posts = Post.where(:group_id => params[:group_id])
+	  @posts = Post.where(:group_id => params[:id])
       render json: { posts: @posts}
 	end
 
