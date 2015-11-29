d281759dcdfc368ed9ef07a2eeb897482376617f
Patrick Schoeps <pschoeps@umich.edu>
2015-11-29 14:30:43 -0500

    ok

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -4,6 +4,10 @@ def destroy
 	@user = User.find_by_auth_token(params[:auth_token]).id
     @relationship = Rrelationship.find_by_follower_id_and_followed_id(@user, params[:followed_id])
     @relationship = @relationship_find.destroy
-    render :json=> {:success=>true}
-  end
+    if @relationship.destroy
+      render :json=> {:success=>true}
+    else
+      render :json => {:success=>false}, :status=>401
+    end
+end
 end
