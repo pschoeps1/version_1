b66898c1ea666bf73786c4e284be2c9ac7980ba4
Patrick Schoeps <pschoeps@umich.edu>
2015-11-30 19:26:11 -0500

    ok

--- app/controllers/api/v1/blocked_users_controller.rb
+++ app/controllers/api/v1/blocked_users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::BlockedUsersController < ApplicationController
 	def create
 
      blocked_user = BlockedUser.create
-     blocked_user.blocker_id = User.find(params[:auth_token]).id
+     blocked_user.blocker_id = User.find_by_auth_token(params[:auth_token]).id
      blocked_user.blocked_id = params[:blocked_id]
       if blocked_user.save
         render :json=> { :success=>true, }, :status=>201
