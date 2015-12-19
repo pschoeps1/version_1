ccc098aad9aaf202d273e08c55f7e768de8de733
Patrick Schoeps <pschoeps@umich.edu>
2015-12-19 11:15:47 -0500

    ok

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,11 +3,11 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
 
   def create
 
-    user = User.new(user_params)
-    #user.name = user_params[:name]
-    #user.password = user_params[:password]
-    #user.password_confirmation = user_params[:password_confirmation]
-    #user.email = user_params[:email]
+    user = User.new
+    user.name = user_params[:name]
+    user.password = user_params[:password]
+    user.password_confirmation = user_params[:password_confirmation]
+    user.email = user_params[:email]
     if user.save
       render :json=> { :success=> true, :auth_token=>user.auth_token, :email=>user.email, :user_id=>user.id, :user_name=>user.username }, :status=>201
       return
@@ -18,6 +18,6 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   end
 
   def user_params
-      params.fetch(:user, {}).permit(:email, :password, :password_confirmation, :name)
+      params.fetch(:user, {}).permit(:email, :password, :password_confirmation, :username)
     end
 end
