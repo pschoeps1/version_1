6565dc664c2fe1891af87a843cc1e95d179cabf6
Patrick Schoeps <pschoeps@umich.edu>
2015-12-28 11:41:13 -0500

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Version1::Application.routes.draw do
         delete 'sign_out' => 'sessions#destroy'
       end
 
-      resources :groups, :only => [:show]
+      resources :groups, :only => [:show, :index]
       resources :posts, :only => [:show]
       resources :flags, :only => [:create]
       resources :blocked_users, :only => [:create]
