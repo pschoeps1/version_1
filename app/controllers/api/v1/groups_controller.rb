84406d59d3b6327053f843d37bb88bac21a044d6
Patrick Schoeps <pschoeps@umich.edu>
2015-12-28 12:09:02 -0500

    ok

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -5,16 +5,24 @@ class Api::V1::GroupsController < ApplicationController
 	end
 
 	def index
+	  user = User.find(params[:user_id])
+	  user_groups = user.groups
 	  if (params[:search])
 	  	puts params[:search]
 	    @groups = Group.search(params[:search]).where(:privacy => false)
 	  else
 	  	@groups = Group.where(:privacy=>false).last(20).reverse
 	  end
+	  new_groups = []
+	  @groups.each do |g|
+	  	unless g.user_id = user.id || Relationship.find(:followed_id=> g.id, :follower_id=> user.id)
+	  		new_groups << g
+	  	end
+	  end
       #@user = current_user
       #@group_show = @user.groups.where(params[:privacy], false)
       #@group_find = Group.find_by_id(params[:id])
-      render :json=> { :groups => @groups }
+      render :json=> { :groups => new_groups}
 	end
 
 end
