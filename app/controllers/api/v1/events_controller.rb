56f0d3e7e5b56b6e5a732e90ab273b96706402ab
Patrick Schoeps <pschoeps@umich.edu>
2016-01-23 15:22:33 -0500

    ok

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,25 +1,6 @@
 class Api::V1::EventsController < ApplicationController
 	respond_to :html, :json
   
-  def update
-    #finds an event and updates it attributes.  Relevant code located in event/show page nested inside a modal.
-    @group = Group.find(params[:group_id])
-    @event = @group.events.find(params[:id]).update_attributes(event_params)
-      respond_to do |format|
-        format.html  { redirect_to group_path(@group) }
-        flash[:success] =  "Event Updated"
-     #   format.js
-    end
-  end
-  
-  def destroy
-    #destroys an event
-    @event = Event.find(params[:id]).destroy
-    if @event.destroy
-      redirect_to group_path(Group.find(params[:group_id]))
-    end
-  end
-  
   def create
   	@user = User.find_by_auth_token(params[:auth_token])
 
@@ -43,8 +24,5 @@ class Api::V1::EventsController < ApplicationController
 
   private
 
-  def event_params
-        params.require(:event).permit(:name, :email, :start_at,
-                                     :end_at, :content, :file)
-  end
+  
 end
