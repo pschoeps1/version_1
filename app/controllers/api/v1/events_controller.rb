2d6a379ee54fb9e45623d8845e5001c6fae1cbfe
Patrick Schoeps <pschoeps@umich.edu>
2016-01-23 15:20:31 -0500

    ok

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -21,14 +21,24 @@ class Api::V1::EventsController < ApplicationController
   end
   
   def create
+  	@user = User.find_by_auth_token(params[:auth_token])
+
+  	if @user
       @group = Group.find(params[:group_id])
-      @event = @group.events.build(event_params)
+      @event = @group.events.build
+      @event.name = params[:name]
+      @event.start_at = params[:start_at]
+      @event.end_at = params[:end_at]
+      @event.content = params[:content]
 
       if @event.save
         render :json => { :success => true }
 	  else
 		render :json => { :success => false }, :status=>401
 	  end
+	else
+		#permission denied
+	end
   end
 
   private
